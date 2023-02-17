4eef803a24a92bd4296f6b2c2593d41a25c281a9
anishverghese <111654668+anishverghese@users.noreply.github.com>
2023-02-17 11:48:19 +0530

    Add files via upload

---
+++ tellptellg.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+
+int main()
+{
+    fstream st;
+    st.open("E:\\anish5.txt", ios::out); // write mode
+    if (!st)
+        cout << "No file";
+
+    else
+    {
+        st << "Hi friends";
+        // position
+        cout << "pointer :" << st.tellp() << endl;
+        st.close();
+    }
+
+    st.open("E:\\anish5.txt", ios::in); // read mode
+    if (!st)
+        cout << "No file";
+
+    else
+    {
+        cout << "pointer : " << st.tellg() << endl;
+        // read
+
+        char ch;
+        while (!st.eof())
+        {
+            cout << "Position " << st.tellg();
+            st >> ch;
+            cout << "character : " << ch << endl;
+        }
+    }
+    return 0;
+}
